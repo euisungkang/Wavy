8f1afa241b0f723009ddd08cb217229179d93bb3
euisungkang <euisungkang@gmail.com>
2024-09-29 19:42:33 -0400

    feat: latest version for aws

---
+++ deploy-commands.ts
@@ -0,0 +1,28 @@
+import {REST, Routes, SlashCommandBuilder} from 'discord.js';
+import { commands } from './commands';
+
+const commandsData = Object.values(commands).map(command => command.data);
+const rest = new REST().setToken(process.env.DISCORD_TOKEN);
+
+type DeployCommandsProps = {
+  guildId: string;
+};
+
+export async function deployCommands({ guildId }: DeployCommandsProps) {
+  try {
+    await rest.put(
+      guildId == ''
+        ? Routes.applicationCommands(process.env.CLIENT_ID)
+        : Routes.applicationGuildCommands(process.env.CLIENT_ID, guildId),
+      {
+        body: commandsData,
+      },
+    );
+
+    for (const command of commandsData) {
+      console.log(`/${command.name}`);
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
